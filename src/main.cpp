7ac3cc5daec837e6d7b0b2fd09348760dc4c8dd9
adechanP <raidomazo@gmail.com>
2022-11-08 16:54:07 +0800

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,72 +26,78 @@ float LH = 0.4;
 float SH = 0.6;
 float H  = 0.8;
 
-void fuzzificationHeight(float x){
+void heightFuzzy(float x){
   for (int i=0; i<5; i++) {
     hMember[i]=0;
   }
   
-  if (x<vShort){
+  if (x<=vShort){
     hMember[0]=1;
   }
-  else if (x<Short_1){
+  else if (x>vShort&&x<=Short_1){
     hMember[0]=(Short_1-x)/abs(Short_1-vShort);      
     hMember[1]=(x-vShort)/abs(Short_1-vShort);
   }
-  else if (x<Short_2){
+  else if (x<=Short_2){
     hMember[1]=1;
   }
-  else if (x<hMedium_1){
+  else if (x>Short_2 && x<=hMedium_1){
     hMember[1]=(hMedium_1-x)/abs(hMedium_1-Short_2); 
     hMember[2]=(x-hMedium_1)/abs(hMedium_1-Short_2);
   }
-  else if (x<hMedium_2){
+  else if (x<=hMedium_2){
     hMember[2]=1;
   }
-  else if (x<Tall_1){
+  else if (x>hMedium_2 && x<=Tall_1){
     hMember[2]=(Tall_1-x)/abs(Tall_1-hMedium_2);     
     hMember[3]=(x-Tall_1)/abs(Tall_1-hMedium_2);
   }
-  else if (x<Tall_2){
+  else if (x<=Tall_2){
     hMember[3]=1;
   }
-  else if (x<vTall){
+  else if (x> Tall_2 && x<=vTall){
     hMember[3]=(vTall-x)/abs(vTall-Tall_2);          
     hMember[4]=(x-vTall)/abs(vTall-Tall_2);
   }
+  else if (x>vTall){
+    hMember[4]=1;
+  }
 }
 
-void fuzzificationWeight(float x){
+void weightFuzzy(float x){
   for (int i=0; i<5; i++){
     wMember[i]=0;
   }
   if (x<vSlim){
     wMember[0]=1;
   }
-  else if (x<Slim_1){
+  else if (x>vSlim && x<=Slim_1){
     wMember[0]=(Slim_1-x)/abs(Slim_1-vSlim);
     wMember[1]=(x-vSlim)/abs(Slim_1-vSlim);
   }
-  else if (x<Slim_2){
+  else if (x<=Slim_2){
     wMember[1]=1;
   }
-  else if (x<wMedium_1){
+  else if (x>Slim_2 && x<=wMedium_1){
     wMember[1]=(wMedium_1-x)/abs(wMedium_1-Slim_2); 
     wMember[2]=(x-wMedium_1)/abs(wMedium_1-Slim_2);
   }
-  else if (x<wMedium_2){
+  else if (x<=wMedium_2){
     wMember[2]=1;
   }
-  else if (x<Heavy_1){
+  else if (x>wMedium_2 && x<=Heavy_1){
     wMember[2]=(Heavy_1-x)/abs(Heavy_1-wMedium_2);  
     wMember[3]=(x-Heavy_1)/abs(Heavy_1-wMedium_2);
   }
-  else if (x<Heavy_2){
+  else if (x<=Heavy_2){
     wMember[3]=1;
   }
-  else if (x<vHeavy){
+  else if (x>Heavy_2 && x<=vHeavy){
     wMember[3]=(vHeavy-x)/abs(vHeavy-Heavy_2);      
     wMember[4]=(x-vHeavy)/abs(vHeavy-Heavy_2);
+  }
+  else if (x>vHeavy){
+    wMember[4]=1;
   }
 }
 
@@ -104,7 +110,6 @@ int rules[5][5]={{3,2,1,0,0},
 void rulesOperation(){
   int k;
   for (int i=0; i<5; i++) {u[i]=0;}
-
   for(int i=0; i<5; i++){
      for(int j=0; j<5; j++){
          k=rules[i][j];
@@ -123,16 +128,16 @@ void defuzzy(){
   }
   healthDecision = num/denum;
   if (healthDecision==U){
-    healthDecisionString = "U";
+    healthDecisionString = "Unhealthy";
   }
   else if (healthDecision==LH){
-    healthDecisionString = "LH";
+    healthDecisionString = "Less Healthy";
   }
   else if (healthDecision==SH){
-    healthDecisionString = "SH";
+    healthDecisionString = "Somewhat Healthy";
   }
   else if (healthDecision==H){
-    healthDecisionString = "H";
+    healthDecisionString = "Healthy";
   }
 }
 
@@ -157,13 +162,13 @@ void loop() {
   lcd.setCursor(0, 1); lcd.print("W: ");
   lcd.setCursor(2, 1); lcd.print(weight);
 
-  fuzzificationHeight(height);
-  fuzzificationWeight(weight);
+  heightFuzzy(height);
+  weightFuzzy(weight);
   rulesOperation();
   defuzzy();
 
-  lcd.setCursor(8, 0); lcd.print("D: ");
-  lcd.setCursor(10, 0); lcd.print(healthDecisionString);
+  lcd.setCursor(6, 0); lcd.print("D: ");
+  lcd.setCursor(8, 0); lcd.print(healthDecisionString);
 
-  delay(10);
+  delay(50);
 }
